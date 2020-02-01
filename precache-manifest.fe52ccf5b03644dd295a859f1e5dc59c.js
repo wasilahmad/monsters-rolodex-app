48e6fcd363b4b5511666564f035488d776ef6105
wasilahmad <wasilinfo@gmail.com>
2020-02-02 01:48:04 +0530

    Updates

---
+++ precache-manifest.fe52ccf5b03644dd295a859f1e5dc59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3ddef47ebae2a63175c5e3fa8d0c8e",
+    "url": "/monsters-rolodex-app/index.html"
+  },
+  {
+    "revision": "3aa83c25d247dcba6cc9",
+    "url": "/monsters-rolodex-app/static/css/main.ba9f2f21.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex-app/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex-app/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa83c25d247dcba6cc9",
+    "url": "/monsters-rolodex-app/static/js/main.9b75496b.chunk.js"
+  },
+  {
+    "revision": "32a7496782ec530cbdef",
+    "url": "/monsters-rolodex-app/static/js/runtime-main.a793a3a3.js"
+  }
+]);
